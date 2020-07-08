ae7eff5eb358e75ccb4d7471da077e14a3223b03
tom_glb <peng_glb@qq.com>
2020-07-08 14:46:23 +0800

    第一次提交

---
+++ src/main/java/com/Git.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+    }
+}
